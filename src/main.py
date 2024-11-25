d74c67377e506cb8957f4a931634947ba3d4dd38
RowanMichell <162051451+RowanMichell@users.noreply.github.com>
2024-11-25 12:16:58 -0600

    descripción de main

--- src/main.py
+++ src/main.py
@@ -4,9 +4,9 @@ import tkinter as tk
 from gui import CompilerApp
 
 def main():
-    root = tk.Tk()
-    app = CompilerApp(root)
-    root.mainloop()
+    root = tk.Tk()          # Costrulle la ventana base para la interfaz grafica 
+    app = CompilerApp(root) # Instancia el objeto que configura la ventana principal 
+    root.mainloop()         # Bucle que atiende los eventos en la interfaz 
 
 if __name__ == "__main__":
     main()
